190c4ef6b08eeeda9dc4e06d8bbc59ff0ae4e6da
Louise <louiselai88@gmail.com>
2014-05-28 09:36:09 +0100

    now show and edit links appear on the index page for the articles?

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
   def update
   	@article = Article.find(params[:id])
 
-  	if @article.update(article_params)
+  	if @article.update
   		redirect_to @article
 		else
 			render 'edit'
