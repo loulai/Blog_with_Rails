842c1db40961fdb395755d2c50f2eacd9075871a
Louise <louiselai88@gmail.com>
2014-05-31 14:31:32 +0100

    added destroying dependencies function for articles model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+	http_basic_authenticate
+
 	def index
 		@articles = Article.all
 	end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
 	validates :title, presence: true, length: { minimum: 3 }
-	has_many :comments
+	has_many :comments, dependent: :destroy
 end
